4b5463e180ad23244b5c97d357c4e2012578c82f
Pedro-HFelix <pf711363@gmail.com>
2024-07-25 18:25:44 -0300

    fix: correct position of styles library

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
       <Html>
-          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-          
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <Head>
+            <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />    
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
