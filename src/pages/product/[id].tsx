a3b6ec416c35a79b7c42687f3df6f06a3972052e
Pedro-HFelix <pf711363@gmail.com>
2024-07-29 14:40:57 -0300

    feat: seo

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -9,6 +9,7 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import Stripe from 'stripe'
 import { useState } from 'react'
 import axios from 'axios'
+import Head from 'next/head'
 
 interface ProductProps {
   product: {
@@ -45,6 +46,9 @@ export default function Product({ product }: ProductProps) {
 
   return (
     <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
       <ProductContainer>
         <ImageContainer>
           <Image src={product.imageUrl} width={520} height={480} alt="" />
